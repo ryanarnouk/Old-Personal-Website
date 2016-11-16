d46687d41b47d97ff3e2d608764770060cefbca0
Ryan <Azbo400@gmail.com>
2016-11-15 21:26:41 -0800

    Worked on the template and the social buttons

--- javascriptstuff.js
+++ javascriptstuff.js
@@ -1,15 +1,43 @@
 //File hooked with index.html
 $(document).ready(function(){
-  //jquery
-  var  mn = $(".main-nav");
-    mns = "main-nav-scrolled";
-    hdr = $('header').height();
+  //Onclick stuff
+  $('.twitterbtn').click(function(){
+    twitter();
+  })
+  $('.youtubebtn').click(function(){
+    youtube();
+  })
+  $('.githubbtn').click(function(){
+    github();
+  })
+  $('.freecodecampbtn').click(function(){
+    freecodecamp();
+  })
+  //Sticky navigation
+  var navOffSet = $("nav").offset().top;;
 
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
-  } else {
-    mn.removeClass(mns);
-  }
-});
+  $("nav").wrap('<div class="nav placeholder"></div>');
+  $(".nav-placeholder").height($("nav").outerHeight);
+
+  $(window).scroll(function(){
+      var scrollPos = $(window).scrollTop();
+
+      if(scrollPos >= navOffSet){
+        $("nav").addClass("fixed");
+      }else{
+        $("nav").removeClass("fixed");
+      }
+  })
 })
+function twitter(){
+  window.open("https://twitter.com/opticodesoft");
+}
+function youtube(){
+  window.open("https://www.youtube.com/channel/UCEgc6SMmlV-H6_qCPwh_m_Q");
+}
+function github(){
+  window.open("https://github.com/Azbo400");
+}
+function freecodecamp(){
+  window.open("https://freecodecamp.com/azbo400");
+}
