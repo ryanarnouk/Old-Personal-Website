859ea41a2999acce7751ebaa77efc7070e4ddde1
azbo400 <azbo400@gmail.com>
2016-12-20 19:55:31 -0800

    Fixed a lot of the bugs

--- javascriptstuff.js
+++ javascriptstuff.js
@@ -41,21 +41,11 @@ $(document).ready(function(){
       }
       $(".portfoliobk").addClass("animated slideInLeft");
 
-      //Navbar
-      $(".button-collapse").sideNav();
   })
   var width = $('.justified').width();
   $('.justified').css('margin-left', '-' + (width / 2) + 'px');
-
-  //Slide nav-bar for mobile devices
-
-  // Initialize collapse button
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
-
   $('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 240
+    menuWidth: 240,
     edge: 'left', // Choose the horizontal origin
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true // Choose whether you can drag to open on touch screens
